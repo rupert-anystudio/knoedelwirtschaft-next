76be65f3b58bb2d3b82df3589b24021ce05cbf43
Rupert Adlmaier <rupert@any.studio>
2022-06-15 14:34:54 +0200

    render data on indexpage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import JsonLog from '../components/JsonLog'
 import { getAppData, getMenuData } from '../lib/api'
 
 export default function Index({ appData, menuData }) {
-  console.log('appData', appData)
-  console.log('menuData', menuData)
+  // console.log('appData', appData)
+  // console.log('menuData', menuData)
   return (
     <div>
+      <JsonLog data={menuData} label='menuData' />
+      <JsonLog data={appData} label='appData' />
     </div>
   )
 }
