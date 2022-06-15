65f0a1c27e281d9815e3ed13e8e14dc9f9883636
Rupert Adlmaier <rupert@any.studio>
2022-06-15 14:34:05 +0200

    temporary jsonlog component

---
+++ components/JsonLog.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+  font-size: 12px;
+  line-height: 1.2;
+  font-family: monospace;
+  background: lavenderblush;
+  display: block;
+  padding: 20px;
+  border-radius: 5px;
+  margin: 10px;
+  code {
+    pre {
+
+    }
+  }
+`
+
+const JsonLog = ({ data, label }) => {
+  if (!data) return null
+  return (
+    <Wrap>
+      {label && <span>{label}</span>}
+      <code>
+        <pre>
+          {JSON.stringify(data, null, 2)}
+        </pre>
+      </code>
+    </Wrap>
+  )
+}
+
+export default JsonLog
